f99433a7c6a40a868dc0c72fc4404a92db7821f0
tejus <tejus.k.v@gmail.com>
2019-05-04 18:20:04 +0530

    Play a video from an HTTP source

--- app/src/main/java/com/tejus/exoplayerdemo/MainActivity.java
+++ app/src/main/java/com/tejus/exoplayerdemo/MainActivity.java
@@ -11,6 +11,7 @@ import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.ui.PlayerView;
 import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
 import com.google.android.exoplayer2.util.Util;
 
 public class MainActivity extends AppCompatActivity {
@@ -26,14 +27,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mPlayerView = findViewById(R.id.player_view);
+        String userAgent = Util.getUserAgent(this, "ExoPlayerDemo");
 
         mPlayer = ExoPlayerFactory.newSimpleInstance(this);
         mPlayerView.setPlayer(mPlayer);
-        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(this,
-                Util.getUserAgent(this, "ExoPlayerDemo"));
+        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(this, userAgent);
         MediaSource mediaSource = new ExtractorMediaSource.Factory(dataSourceFactory)
                 .createMediaSource(Uri.parse("asset:///-intro-creampie.mp4"));
-        mPlayer.prepare(mediaSource);
-        mPlayer.setPlayWhenReady(true);
+
+        DefaultHttpDataSourceFactory httpSourceFactory = new DefaultHttpDataSourceFactory(userAgent);
+        ExtractorMediaSource extractorMediaSource = new ExtractorMediaSource.Factory(httpSourceFactory)
+                .createMediaSource(Uri.parse("https://d17h27t6h515a5.cloudfront.net/topher/2017/April/58ffd974_-intro-creampie/-intro-creampie.mp4"));
+        mPlayer.prepare(extractorMediaSource);
+        //mPlayer.setPlayWhenReady(true);
     }
 }
