13eea413cc0d9a7aeab756e971eefa8c6825f886
tejus <tejus.k.v@gmail.com>
2019-05-04 15:04:31 +0530

    Play the video from assets folder

--- app/src/main/java/com/tejus/exoplayerdemo/MainActivity.java
+++ app/src/main/java/com/tejus/exoplayerdemo/MainActivity.java
@@ -1,13 +1,39 @@
 package com.tejus.exoplayerdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    private PlayerView mPlayerView;
+    private SimpleExoPlayer mPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mPlayerView = findViewById(R.id.player_view);
+
+        mPlayer = ExoPlayerFactory.newSimpleInstance(this);
+        mPlayerView.setPlayer(mPlayer);
+        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(this,
+                Util.getUserAgent(this, "ExoPlayerDemo"));
+        MediaSource mediaSource = new ExtractorMediaSource.Factory(dataSourceFactory)
+                .createMediaSource(Uri.parse("asset:///-intro-creampie.mp4"));
+        mPlayer.prepare(mediaSource);
+        mPlayer.setPlayWhenReady(true);
     }
 }
